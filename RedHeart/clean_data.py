e4e821c15139f5a133a4f95ba8e14c170baf29c9
TangyKiwi <orange.panda.65@gmail.com>
2022-06-20 11:46:29 -0700

    Clean context

--- RedHeart/clean_data.py
+++ RedHeart/clean_data.py
@@ -3,17 +3,25 @@ from pathlib import Path
 
 df = pd.read_csv("https://raw.githubusercontent.com/TangyKiwi/Worldie/master/RedHeart/redheart_data.csv")
 
+# Age
 # change all non numeric age values to 0 if under 1 year, -1 if unknown
 for i in range(len(df["age"])):
     if not df["age"][i].isnumeric():
         if df["age"][i] == "Unknown": df.at[i, "age"] = -1
         else: df.at[i, "age"] = 0
 
+# Gender
 # standardize all gender types, strip() bc of "male" and "male " instances
 # df.replace to fix all typos, "associate violence" case should be "male"
 df["gender"] = df["gender"].str.lower()
 df["gender"] = df["gender"].str.strip()
 df = df.replace({"gender": {"male, fema":"male, female", "unknown gender":"unknown", "associate violence":"male"}})
+
+# Context
+# df.replace to fix all typos
+df["context"] = df["context"].str.lower()
+df["context"] = df["context"].str.strip()
+df = df.replace({"unknown context":"unknown", "unsolved":"unknown", "unknown context of death":"unknown", "incomplete entry":"unknown", "domestic violece":"domestic violence", "associate violece":"associate violence"})
 
 filepath = Path("redheart_data_cleaned.csv")
 filepath.parent.mkdir(parents=True, exist_ok=True)
