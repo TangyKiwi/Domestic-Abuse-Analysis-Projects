ff8afd6742a7d55f0213e51b3fc89bae8a911298
TangyKiwi <orange.panda.65@gmail.com>
2022-06-20 12:34:17 -0700

    Add age group graphs & context graphs

--- RedHeart/plot_data.py
+++ RedHeart/plot_data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 st.write("""
 # Red Heart Campaign Data Visualization
@@ -21,16 +22,27 @@ age = df.get("age")
 age = list(map(int, age))
 
 s = pd.DataFrame({"Age":age})["Age"].value_counts()
-df_age = pd.DataFrame({"Age":s.index, "Count":s.values})
-fig = px.bar(df_age, x="Age", y="Count", title="Age")
+age_counts = pd.DataFrame({"Age":s.index, "Count":s.values})
+fig = px.bar(age_counts, x="Age", y="Count", title="Age")
+st.plotly_chart(fig)
+age_groups = pd.cut(age, bins=[-2, -1, 14, 24, 64, 100])
+# (-2, -1] (-1, 14] (14, 24] (24, 64] (64, 100]
+unique, counts = np.unique(age_groups, return_counts=True)
+age_groups = dict(zip(unique, counts))
+age_groups = pd.DataFrame({"Age Groups":["Unknown", "Children", "Youth", "Adults", "Seniors"], "Count":age_groups.values()})
+fig = px.bar(age_groups, x="Age Groups", y="Count", title="Age Groups")
+st.plotly_chart(fig)
+fig = px.pie(age_groups, values="Count", names="Age Groups", title="Age Groups")
 st.plotly_chart(fig)
 
 ###############################
 #           Context           #
 ###############################
 s = df["context"].value_counts()
-df_context = pd.DataFrame({"Context":s.index, "Count":s.values})
-fig = px.bar(df_context, x="Context", y="Count", title="Context")
+context_counts = pd.DataFrame({"Context":s.index, "Count":s.values})
+fig = px.bar(context_counts, x="Context", y="Count", title="Context")
+st.plotly_chart(fig)
+fig = px.pie(context_counts, values="Count", names="Context", title="Context")
 st.plotly_chart(fig)
 
 ############################
@@ -39,8 +51,8 @@ st.plotly_chart(fig)
 year = df.get("year")
 year = list(map(int, year))
 s = pd.DataFrame({"Year":year})["Year"].value_counts()
-df_year = pd.DataFrame({"Year":s.index, "Count":s.values})
-fig = px.bar(df_year, x="Year", y="Count", title="Year")
+year_counts = pd.DataFrame({"Year":s.index, "Count":s.values})
+fig = px.bar(year_counts, x="Year", y="Count", title="Year")
 st.plotly_chart(fig)
 
 ######################################
@@ -48,8 +60,8 @@ st.plotly_chart(fig)
 ######################################
 gender = df.get("gender")
 s = pd.DataFrame({"Accused Gender":gender})["Accused Gender"].value_counts()
-df_gender = pd.DataFrame({"Accused Gender":s.index, "Count":s.values})
-fig = px.bar(df_gender, x="Accused Gender", y="Count", title="Accused Gender")
+gender_counts = pd.DataFrame({"Accused Gender":s.index, "Count":s.values})
+fig = px.bar(gender_counts, x="Accused Gender", y="Count", title="Accused Gender")
 st.plotly_chart(fig)
-fig = px.pie(df_gender, values="Count", names="Accused Gender", title="Accused Gender")
+fig = px.pie(gender_counts, values="Count", names="Accused Gender", title="Accused Gender")
 st.plotly_chart(fig)
