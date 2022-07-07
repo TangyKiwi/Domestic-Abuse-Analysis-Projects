84607a6dad97fcad127557dec48d4e1f8d607ef4
TangyKiwi <orange.panda.65@gmail.com>
2022-07-06 18:18:16 -0700

    Add Facebook scraper

---
+++ Social Media Scraper/facebook.py
@@ -0,0 +1,8 @@
+from facebook_scraper import get_posts
+from facebook_scraper import get_profile
+
+# for post in get_posts('nintendo', pages=1):
+#     print(post['text'][:50])
+
+# "Meta" has heavy rate limiting + IP banning as well like Instagram
+get_profile("10111007623865331")
